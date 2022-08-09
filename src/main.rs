b007611b3db691fc0194cfe580e95856a3bd692d
Anders Qvist <anders.qvist@xenit.se>
2022-08-09 16:23:23 +0200

    Count requests for a token.

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,30 @@ use actix_web::{
 use dashmap::DashMap;
 use rand::Rng;
 use serde::{Deserialize, Serialize};
-use std::sync::Arc;
+use std::sync::{Arc, atomic::AtomicU64};
 
-type Cache = Arc<DashMap<String, String>>;
+type Cache = Arc<DashMap<String, Entry>>;
+
+struct Entry {
+    target: String,
+    counter: AtomicU64,
+}
+
+impl From<Registration> for Entry {
+    fn from(reg: Registration) -> Self {
+        Entry { target: reg.target, counter: AtomicU64::new(0) }
+    }
+}
 
 #[get("/{token}")]
 async fn redirect(
     token: web::Path<String>,
     store: web::Data<Cache>,
 ) -> impl Responder {
-    if let Some(target) = store.get(token.as_str()) {
+    if let Some(entry) = store.get(token.as_str()) {
+        entry.counter.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
         HttpResponse::TemporaryRedirect()
-            .append_header((header::LOCATION, target.to_owned()))
+            .append_header((header::LOCATION, entry.target.to_owned()))
             .finish()
     } else {
         HttpResponse::NotFound().finish()
@@ -51,10 +63,12 @@ async fn create(
     if let Some(_) = store.get(token.as_str()) {
         return HttpResponse::InternalServerError().finish();
     }
-    store.insert(token.clone(), registration.target.clone());
+    let target = registration.target.clone();
+    let entry = Entry::from(registration.into_inner());
+    store.insert(token.clone(), entry);
     HttpResponse::Created().json(RegistrationResponse {
         token,
-        target: registration.target.clone(),
+        target,
     })
 }
 
